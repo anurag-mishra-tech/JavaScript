a9f0c3fddcfdf2e4460e3a3eb082806db8d86889
Anurag Mishra <am91294@gmail.com>
2023-11-24 21:13:13 +0530

    conversion operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "33abc" 
+
+console.log(typeof score);
+console.log(typeof(score))
+
+
+// converting to Number
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" => 33 
+// "33abc" => NaN
+// true => 1 ; false => 0 
+
+let isLoggedIn = "sdfsdf"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+// 1 => true ; 0 => false
+// "" => false 
+// "anurag" => true
+
+let someNumber = 33 
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
