31f70b085d33081e0c65aa21a818413ae15bf899
Anurag Mishra <am91294@gmail.com>
2023-11-24 20:52:24 +0530

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -5,3 +5,24 @@
 console.log( 3+3 ) 
 
 console.log("Anurag") //code readability should be high 
+
+let name = "anurag"
+let age = 29
+let isLoggedIn = false
+
+/* Primitive Datatypes */
+
+// number => 2^53
+// bigint  
+// string => "" //prefer in double quotes 
+// boolean  => true/false
+// null => standalone value and datatypes
+// undefined 
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof "anurag")
+console.log(typeof age)
+console.log(typeof null)
